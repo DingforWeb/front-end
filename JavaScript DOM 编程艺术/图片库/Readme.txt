0192eeb871a6eeb7902ff73c85d6841a2b83d007
DingforWeb <mubaid@163.com>
2016-07-15 19:21:34 +0800

    2016/7/15

--- JavaScript DOM 编程艺术/图片库/Readme.txt
+++ JavaScript DOM 编程艺术/图片库/Readme.txt
@@ -1,3 +1,30 @@
-ע�⣺
-������ǿ��ƽ���˻�
-ʹ�ṹ����ʾ����Ϊ����
+
+（一）共享onload事件
+网页加载完毕时会触发onload事件，这个事件与window对象相关联。当一个函数必须在网页加载完后执行时，例如prepareGallery函数，可以使用：
+window.onload=prepareGallery;
+当有多个函数都在页面加载时得到执行，如：
+window.onload=firstFunction;
+window.onload=secondFunction;
+secondFunction将取代firstFunction，每个事件处理函数只能绑定一条指令。
+function addLoadEvent(func){
+	var oldonload=window.onload;
+	if(typeof window.onload!='function'){
+		window.onload=func;
+	}else{
+		window.onload=function(){
+			oldonload();
+			func();
+		}
+	}
+}
+参数func：打算在页面加载完毕时执行的函数的名字
+1.把现有的window.onload事件处理函数的指存入变量oldonload
+2.如果在这个处理函数上还没有绑定任何函数，则将新函数添加给它
+3.如果这个处理函数上已经绑定了一些函数，则将新函数追加到指令的末尾。
+
+（二）平稳退化
+当浏览器不支持javascript时，即使某些功能不能使用，但最基本的操作仍然可以运行。
+慎用“javascript:”伪协议与#标记。
+
+（三）结构与行为分离
+文档的结构与行为应该分开，将onclick写入函数中，而不是html文件中
--- JavaScript DOM 编程艺术/图片库/js/gallery.js
+++ JavaScript DOM 编程艺术/图片库/js/gallery.js
@@ -1,5 +1,5 @@
 // JavaScript Document
-
+//确认网页加载完成后执行函数
 function addLoadEvent(func){
 	var oldonload=window.onload;
 	if(typeof window.onload!='function'){
@@ -21,41 +21,54 @@ function insertAfter(newElement,targetElement){
 		parent.insertBefore(newElement,targetElement.nextSibling);
 	}
 }
-
+//将只为gallery.js服务的图片与文字,与html分开.所以使用DOM方法来创建
 function preparePlaceholder(){
+//	检查方法
 	if(!document.createElement) return false;
 	if(!document.createTextNode) return false;
 	if(!document.getElementById) return false;
 	if(!document.getElementById("imagegallery")) return false;
+//	创建img元素节点,设置id属性为placeholder,设置src属性,设置alt属性.
 	var placeholder=document.createElement("img");
 	placeholder.setAttribute("id" ,"placeholder");
 	placeholder.setAttribute("src","images/Chrysanthemum.jpg");
 	placeholder.setAttribute("alt","my image gallery");
+//	创建p元素节点,设置id为description,创建文本节点,追加到P上
 	var description=document.createElement("p");
 	description.setAttribute("id","description");
 	var destext=document.createTextNode("Choose an image");
 	description.appendChild(destext);
+//	将img元素与p元素插入文档中
 	var gallery=document.getElementById("imagegallery");
 	insertAfter(placeholder,gallery);
 	insertAfter(description,placeholder);
 }
 
 function prepareGallery(){
+//	向后兼容,确认浏览器是否理解这些方法
 	if(!document.getElementsByTagName) return false;
 	if(!document.getElementById) return false;
 	if(!document.getElementById("imagegallery")) return false;
+//	遍历imagegallery元素中的所有连接
 	var gallery=document.getElementById("imagegallery");
 	var links=gallery.getElementsByTagName("a");
+//	遍历元素,添加onclick事件函数,改变其行为,就不用在html中添加事件函数了
 	for (var i=0;i<links.length;i++){
 		links[i].onclick=function(){
+//			this代表了此刻与onclick方法相关联的那个元素
 			return showPic(this);
 		}
+//		键盘访问
 		links[i].onKeypress=links[i].onclick;
 	}
 }
 
+//负责:1.找出id为placeholder的图片并修改其src属性2.找出id为description的元素
+//并修改其第一个子元素的nodeValue属性
 function showPic(whichpic){
+//	检查特定元素是否存在
 	if(!document.getElementById("placeholder")) return false;
+//	修改图片
 	var source=whichpic.getAttribute("href");
 	var placeholder=document.getElementById("placeholder");
 	placeholder.setAttribute("src",source);
@@ -67,6 +80,7 @@ function showPic(whichpic){
 		text="";
 	}
 	var description=document.getElementById("description");
+//	文本节点的nodeType属性为3
 	if(description.firstChild.nodeType==3){
 		description.firstChild.nodeValue=text;
 	}
